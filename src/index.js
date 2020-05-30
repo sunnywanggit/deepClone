7ed3cc8d57ff3d18b132b05ad02b2d268a80eebc
ergouwang <mrergouwang@gmail.com>
2020-05-30 16:34:03 +0800

    重构代码

--- src/index.js
+++ src/index.js
@@ -2,29 +2,23 @@ function deepClone(source) {
   //判断source是不是一个对象
   if (source instanceof Object) {
     //如果source是一个数组
+    let dist 
     if (source instanceof Array) {
-      const dist = new Array();
-      for (let key in source) {
-        dist[key] = deepClone(source[key]);
-      }
-      return dist;
+      dist = new Array();
       //如果source是一个函数
     } else if (source instanceof Function) {
-      const dist = function () {
+      dist = function () {
         return source.apply(this, arguments);
       };
-
-      for (let key in source) {
-        dist[key] = deepClone(source[key]);
-      }
-      return dist;
     } else {
-      const dist = new Object();
-      for (let key in source) {
-        dist[key] = deepClone(source[key]);
-      }
-      return dist;
+      dist = new Object();
     }
+
+    for (let key in source) {
+      dist[key] = deepClone(source[key]);
+    }
+    return dist;
+
   }
   return source;
 }
