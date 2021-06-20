3ac2e280dc89b4997f96a2db499ae0668cabd7ef
wangzhen <wangzhen1@ijovo.com>
2021-06-21 00:07:41 +0800

    refactor: 添加函数注释

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 class DeepClone {
   constructor() {
+    // 缓存变量，用来检测环引用
+    // 之所以放在构造器中，是为了解决 catch 被全局共享，造成互相污染的问题
     this.cache = [];
   }
   clone(source) {
@@ -22,8 +24,10 @@ class DeepClone {
         } else {
           dist = new Object();
         }
+        // 这里在存入的时候，原来的”环节点“和复制后的”环节点”都需要存入
         this.cache.push([source, dist]);
         for (let key in source) {
+          // 这里的操作是为了避免拷贝原型上的属性
           if (source.hasOwnProperty(key)) {
             dist[key] = this.clone(source[key]);
           }
@@ -36,6 +40,8 @@ class DeepClone {
   }
   findCache(source) {
     for (let i = 0; i < this.cache.length; i++) {
+      // 我们在检测环引用的时候，要检测的是原来的“环节点”而要抛出的是复制后的“环节点”
+      // 这里如果还不是特别理解的话，下来可以画一张图好好理解一下
       if (this.cache[i][0] === source) {
         return this.cache[i][1];
       }
